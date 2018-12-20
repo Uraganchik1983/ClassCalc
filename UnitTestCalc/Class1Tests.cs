c76aaee8eb8f7a39d24672a223adca5d1137effb
Uraganchik1983 <capability@capability.org.ua>
2018-12-20 23:20:59 +0200

    add auto unit test

---
+++ UnitTestCalc/Class1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassCalc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCalc.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void SummTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
